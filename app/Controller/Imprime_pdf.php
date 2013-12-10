3abd8fa2eab097850f1f3d042c5ea2dc49db2229
Caren_UFSM <kp13@pop.com.br>
2013-12-10 10:57:37 -0200

    --

--- app/Controller/Imprime_pdf.php
+++ app/Controller/Imprime_pdf.php
@@ -1,26 +1,27 @@
 <?php
+App::uses('AppController', 'Controller');
 
-function viewPdf($id = null) 
-    { 
-        if (!$id) 
-        { 
-            $this->Session->setFlash('Sorry, there was no property ID submitted.'); 
-            $this->redirect(array('action'=>'index'), null, true); 
+class Imprime_pdf extends AppController {
+    
+    function viewPdf($id = null){ 
+            if (!$id){ 
+                $this->Session->setFlash('Sorry, there was no property ID submitted.'); 
+                $this->redirect(array('action'=>'index'), null, true); 
+            } 
+            Configure::write('debug',0); // Otherwise we cannot use this method while developing 
+
+            $id = intval($id); 
+
+            $property = $this->__view($id); // here the data is pulled from the database and set for the view 
+
+            if (empty($property)) 
+            { 
+                $this->Session->setFlash('Sorry, there is no property with the submitted ID.'); 
+                $this->redirect(array('action'=>'index'), null, true); 
+            } 
+
+            $this->layout = 'pdf'; //this will use the pdf.ctp layout 
+            $this->render(); 
         } 
-        Configure::write('debug',0); // Otherwise we cannot use this method while developing 
-
-        $id = intval($id); 
-
-        $property = $this->__view($id); // here the data is pulled from the database and set for the view 
-
-        if (empty($property)) 
-        { 
-            $this->Session->setFlash('Sorry, there is no property with the submitted ID.'); 
-            $this->redirect(array('action'=>'index'), null, true); 
-        } 
-
-        $this->layout = 'pdf'; //this will use the pdf.ctp layout 
-        $this->render(); 
-    } 
-
+}
 ?>
